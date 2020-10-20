7899843c40205c41345a7b91b6a03175f70b9011
MUNMUN <munakond1@gmail.com>
2020-10-20 16:56:36 +0100

    webexampl

--- webexampl/radiusnum.php
+++ webexampl/radiusnum.php
@@ -4,9 +4,10 @@ class MyCircle
     public $radius;
 
     
-    function __construct($radius)
-    {
-        $this->radius = $radius;
+    
+    function __construct()
+   {
+        $this->radius = 1;
     }
     function __desctuct ()
     {
@@ -24,9 +25,11 @@ class MyCircle
         return $area = 3.1416*(2*$this->radius);
     }
 }
-$Circle1 = new MyCircle(4);
-$Circle2 = new MyCircle(2);
+$Circle1 = new MyCircle();
+$Circle1->set_Radius(5);
+$Circle2 = new MyCircle();
 $Circle2->set_Radius(5);
+
 echo "First Circle : ".$Circle1->get_Area();
 echo "<br>";
 echo "Second Circle : ".$Circle2->get_Area();
